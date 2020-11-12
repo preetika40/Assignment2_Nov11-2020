63c8b6c19446eda067c3d161602c6afdb2809f09
preetika40 <preetika.singh40@gmail.com>
2020-11-12 18:24:57 +0530

    Update V1

--- scripts/index.js
+++ scripts/index.js
@@ -1,64 +1,65 @@
-//for pause button
-var c = document.getElementById("pause-btn");
-var ctx = c.getContext("2d");
-ctx.fillStyle = "red";
-ctx.moveTo(15,15);
-ctx.lineTo(15,35);
-ctx.lineTo(20,35);
-ctx.lineTo(20,15);
-ctx.lineTo(15,15);
-ctx.moveTo(30,15);
-ctx.lineTo(30,35);
-ctx.lineTo(35,35);
-ctx.lineTo(35,15);
-ctx.lineTo(30,15);
-ctx.stroke();
-ctx.fillStyle = "white";
-ctx.fill();
+let c, ctx;
+//Display pause button using canvas
+(() => {
+    c = document.querySelector(".pause-btn");
+    ctx = c.getContext("2d");
+    ctx.moveTo(15,15);
+    ctx.lineTo(15,35); 
+    ctx.lineTo(20,35);
+    ctx.lineTo(20,15);
+    ctx.lineTo(15,15);
+    ctx.moveTo(30,15);
+    ctx.lineTo(30,35);
+    ctx.lineTo(35,35);
+    ctx.lineTo(35,15);
+    ctx.lineTo(30,15);
+    ctx.stroke();
+    ctx.fillStyle = "white";
+    ctx.fill();
+})();
 
-//for play button
-var c = document.getElementById("play-btn");
-var ctx = c.getContext("2d");
-ctx.moveTo(10,10);
-ctx.lineTo(10,40);
-ctx.lineTo(45,25);
-ctx.lineTo(10,10);
-ctx.stroke();
-ctx.fillStyle = "red";
-ctx.fill();
+//Display pause button using canvas
+(() => {
+    c = document.querySelector(".play-btn");
+    ctx = c.getContext("2d");
+    ctx.moveTo(10,10);
+    ctx.lineTo(10,40);
+    ctx.lineTo(45,25);
+    ctx.lineTo(10,10);
+    ctx.stroke();
+    ctx.fillStyle = "red";
+    ctx.fill();
+})();
 
-//for stop button
-var c = document.getElementById("stop-btn");
-var ctx = c.getContext("2d");
-ctx.moveTo(15,15);
-ctx.lineTo(15,35);
-ctx.lineTo(35,35);
-ctx.lineTo(35,15);
-ctx.stroke();
-ctx.fillStyle = "white";
-ctx.fill();
+////Display stop button using canvas
+(() => {
+    c = document.querySelector(".stop-btn");
+    ctx = c.getContext("2d");
+    ctx.moveTo(15,15);
+    ctx.lineTo(15,35);
+    ctx.lineTo(35,35);
+    ctx.lineTo(35,15);
+    ctx.stroke();
+    ctx.fillStyle = "white";
+    ctx.fill();
+})();
 
-//toggle function
-function toggle(){
-    var hideVideo = document.getElementById("hide-video");
-    var showVideo = document.getElementById("show-video");
-    var hideContent = document.getElementById("hide-content");
-    var showContent = document.getElementById("show-content");
-    var toggleBtn = document.getElementById("toggle-btn");
 
-    if (showVideo.style.display === "none" && showContent.style.display === "none"){
-        hideVideo.style.display = "none";
-        hideContent.style.display = "none";
-        showVideo.style.display = "block";
-        showContent.style.display = "block";
-        toggleBtn.innerHTML = "Hide Content";
+//toggle function to hide/show data
+
+const btn = document.querySelector('.toggle-btn');
+btn.addEventListener('click', () => {
+    document.querySelector(".hide-video").classList.toggle('hide');
+    document.querySelector(".hide-content").classList.toggle('hide');
+    document.querySelector(".show-video").classList.toggle('show');
+    document.querySelector(".show-content").classList.toggle('show');
+    if (btn.innerHTML === "Show content")
+    {
+        btn.innerHTML = "Hide content"; 
     }
-
-    else{
-        hideVideo.style.display = "block";
-        hideContent.style.display = "block";
-        showVideo.style.display = "none";
-        showContent.style.display = "none";
-        toggleBtn.innerHTML = "Show Content";
+    else
+    {
+        btn.innerHTML = "Show content";
     }
-}
+    
+})
